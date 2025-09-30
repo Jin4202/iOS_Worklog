047eff7fcf35da96661833f17d5e691d06c51805
Jin4202 <jinseokheo.se@gmail.com>
2025-09-29 23:01:31 -0400

    Initial Commit

---
+++ Worklog/WorklogApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorklogApp.swift
+//  Worklog
+//
+//  Created by Jinseok Heo on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct WorklogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
